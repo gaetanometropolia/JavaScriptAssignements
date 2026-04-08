66e72307201dcd5263b5a5d87fa041e6c8d4a9d5
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-04-08 22:43:39 +0300

    tested and success

--- Module 2 Assignements JavaScript/4/4.js
+++ Module 2 Assignements JavaScript/4/4.js
@@ -1,1 +1,21 @@
 "use strict";
+// Write a program that asks the user for numbers until the user gives zero. The given numbers are printed in the console from the largest to the smallest. (2p)
+
+const numbers=[];
+
+let input;
+
+while (true){
+    input=parseInt(prompt("enter a number, press 0 to exit"));
+    if(input===0){
+        break;
+    }
+    numbers.push(input);
+}
+
+numbers.sort((a, b) =>  b - a );
+
+
+for(let j=0; j<numbers.length; j++){
+    console.log(numbers[j]);
+}
--- Module 2 Assignements JavaScript/5/5.js
+++ Module 2 Assignements JavaScript/5/5.js
@@ -1,1 +1,21 @@
 "use strict";
+// Write a program that prompts the user for numbers. When the user enters one of the numbers previously entered, the program will announce that the number has already been given and stops its operation and then prints all the given numbers to the console in ascending order. (2p)
+
+const numbers=[];
+
+let input;
+
+while(true){
+    input=parseInt(prompt("Type a number. If you enter one of the numbers previously entered, the program will announce that the number has already been given and stops "));
+    if (numbers.includes(input)){
+        alert(`The number ${input} has already given`)
+        break;
+    }
+    numbers.push(input);
+}
+
+numbers.sort((a,b) => a - b);
+
+for(let j=0; j<numbers.length; j++){
+    console.log(numbers[j]);
+}
