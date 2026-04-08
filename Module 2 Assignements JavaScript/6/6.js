cf6838870c2743e40756e6ceb5b1e89bc6a38457
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-04-08 22:54:43 +0300

    tested and success

--- Module 2 Assignements JavaScript/6/6.js
+++ Module 2 Assignements JavaScript/6/6.js
@@ -1,1 +1,18 @@
 "use strict";
+// Write a function that returns a random dice roll between 1 and 6. The function should not have any parameters. Write a main program that rolls the dice until the result is 6. The main program should print out the result of each roll in an unordered list (<ul>). (2p)
+
+function rollDice(){
+    return Math.floor(Math.random()*6)+1;
+}
+
+let listHTML="<ul>";
+let result = 0;
+
+while (result!==6){
+    result=rollDice();
+    listHTML += `<li>Roll result: ${result}</li>`;
+}
+
+listHTML += "</ul>";
+
+document.querySelector("#target").innerHTML=listHTML
